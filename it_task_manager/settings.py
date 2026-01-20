70fe835ac66daf58fa34eebd8ec83bfc2cc55b9b
Maksym Dazidov <maksymdazidov@gmail.com>
2026-01-20 14:15:01 +0100

    Move SECRET_KEY to environment variables

--- it_task_manager/settings.py
+++ it_task_manager/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p4d0@54w+ri+t%vv8mc4xrs44j9s+hulp4_dgk*2n+-zesv1@v'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -121,5 +125,5 @@ STATIC_URL = 'static/'
 AUTH_USER_MODEL = 'tasks.Worker'
 
 LOGIN_URL = 'login'
-LOGIN_REDIRECT_URL = 'tasks:index'
+LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = 'login'
